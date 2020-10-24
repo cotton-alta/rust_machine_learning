f7b70fdf842f8746720d4a27119da1e0b6ddddd4
cotton <chinoknct@gmail.com>
2020-10-24 23:11:43 +0900

    [update]gridに関する処理を構造体へ移行

--- q_learning/src/main.rs
+++ q_learning/src/main.rs
@@ -1,28 +1,45 @@
 use std::collections::HashMap;
 
 fn main() {
+    #[derive(Debug)]
+    struct Grid {
+        field: [[i32; 4]; 5],
+        field_type: HashMap<String, i32>,
+        actions: HashMap<String, i32>,
+        agent_position: [i32; 2],
+    }
+
+    impl Grid {
+        fn position_change(&mut self, x: i32, y: i32) {
+            self.agent_position = [x, y]
+        }
+    }
+
     // gridに関する処理
-    let grid: [[i32; 4]; 5] = [[0; 4]; 5];
-    println!("{:?}", grid);
+    let mut grid = Grid {
+        field: [[0; 4]; 5],
+        field_type: HashMap::new(),
+        actions: HashMap::new(),
+        agent_position: [0, 4],
+    };
 
-    let mut field_type = HashMap::new();
-    field_type.insert("normal", 0);
-    field_type.insert("goal", 1);
-    field_type.insert("wall", 2);
-    field_type.insert("trap", 3);
+    grid.field_type.insert(String::from("normal"), 0);
+    grid.field_type.insert(String::from("goal"), 1);
+    grid.field_type.insert(String::from("wall"), 2);
+    grid.field_type.insert(String::from("trap"), 3);
 
-    let mut actions = HashMap::new();
-    actions.insert("up", 0);
-    actions.insert("down", 1);
-    actions.insert("left", 2);
-    actions.insert("right", 3);
+    grid.actions.insert(String::from("up"), 0);
+    grid.actions.insert(String::from("down"), 1);
+    grid.actions.insert(String::from("left"), 2);
+    grid.actions.insert(String::from("right"), 3);
 
-    let start_position = [0, 4];
-    let agent_position = [0, 4];
+    grid.position_change(1, 1);
 
     // agentに関する処理
+    println!("{:?}", grid);
+    step();
 }
 
 fn step() {
-
+    println!("step function");
 }
